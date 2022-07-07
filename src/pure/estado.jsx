1df6110e2e61dded08c05f6a98a9ea6bb27bc631
Alejandro Ventura Fuentes <aledar16@gmail.com>
2022-07-06 20:09:18 -0400

    Completo

--- src/pure/estado.jsx
+++ src/pure/estado.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Newuser } from '../models/contacto.class';
 
 
 const Estadocomplement = ({newuser}) => {
+
+    //const [conectado, setConectado] = useState(newuser);
+    ///const state_user = () => { /* to be added */};
+    ///onClick={() => setConectado(!conectado)} conectado === false ? 'Conectado' : 'Desconectado'
+
+    const [conectado, setConectado] = useState(newuser.conectado);
+    const newstate = () => { 
+        setConectado(!conectado) 
+    }
+
     return (
         <div>
             <h2>Nombre: { newuser.nombre }</h2>
@@ -12,11 +22,11 @@ const Estadocomplement = ({newuser}) => {
                  
             <h2>Email: { newuser.email}</h2>
                 
-            <h2>Estado: { 
-                newuser.conectado ? 
-                    <p style={{ color: 'green' }}>'Contacto En Línea'</p>:
-                    <p style={{ color: 'red' }}>'Contacto No Disponible'</p>
-            }</h2>
+            <h2>Estado: { conectado ?
+                <p style={{ color: 'green' }}>Contacto En Línea</p>:
+                <p style={{ color: 'red' }}>Contacto No Disponible</p>}</h2>
+
+            <button onClick={newstate}>CLICK</button>
         </div>
     );
 };
